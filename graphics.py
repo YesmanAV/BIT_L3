e809a40c558142d875a9ff8ceb85c637b4629be7
Trashkov-PO <pashawit@gmail.com>
2021-10-27 21:30:40 +0300

    Модернизация

--- graphics.py
+++ graphics.py
@@ -4,37 +4,55 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 
 
+def display(im):
+    img = im.resize((750, 400))
+    panel = Label(image=img)
+    panel.image = img
+    panel.place(x=670, y=100)
+
+
 class MyWindow:
 
     def __init__(self, **kw):
         super().__init__(**kw)
-        btn1 = Button(text="Выбрать картинку", background="green", foreground="black",
+        self.panel = Label()
+        self.img = None
+        self.btn1 = Button(text="Выбрать картинку", background="grey", foreground="black",
                       padx="200", pady="7", font="13", command=self.open_img)
-        btn1.place(x=50, y=20)
-        btn2 = Button(text="Нанести водяной знак", background="green", foreground="black",
-                      padx="200", pady="7", font="13")
-        btn2.place(x=760, y=20)
-        self.box = Listbox(width=90, height=30)
-        self.box.place(x=50, y=100)
+        self.btn1.place(x=50, y=20)
+        self.btn2 = Button(text="Нанести водяной знак", background="grey", foreground="black",
+                      padx="186", pady="7", font="13")
+        self.btn2.place(x=50, y=80)
+        self.box = Listbox(width=90, height=20)
+        self.box.place(x=50, y=150)
+        self.path1 = ''
 
     def open_img(self):
+        if self.path1 != '':
+            self.clear_label_image()
+        self.box.delete(0, END)
         try:
-            path = filedialog.askopenfilename(title='open')
+            path = filedialog.askopenfilename(title='Открыть')
             if path:
-                img = ImageTk.PhotoImage(Image.open(path).resize((750, 400)))
-                panel = Label(image=img)
-                panel.image = img
-                panel.place(x=670, y=100)
+                self.path1 = path
+                self.img = ImageTk.PhotoImage(Image.open(path).resize((610, 400)))
+                self.panel = Label(image=self.img)
+                self.panel.image = self.img
+                self.panel.place(x=650, y=40)
             else:
                 self.box.insert(END, 'ERROR:    Картинка не выбрана')
-                print('Картинка не выбрана')
         except Exception as ex:
-            self.box.insert(END, 'ERROR:' + str(ex))
+            self.box.insert(END, 'ERROR:    ' + str(ex))
 
+    def clear_label_image(self):
+        self.panel.destroy()
+
+    def send_image(self):
+        print('Code')
 
 
 root = Tk()
-root.geometry("1500x600")
+root.geometry("1300x500")
 root.resizable(width=False, height=False)
 root.title("Watermarking an image")
 MyWindow()
