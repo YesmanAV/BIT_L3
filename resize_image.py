37b6ebd5489b49ae307c0f8eaf86b5ac73d44258
VAyalov <vasyabylba@mail.ru>
2021-11-11 21:31:04 +0300

    Finding the optimal size

--- resize_image.py
+++ resize_image.py
@@ -3,3 +3,8 @@ from PIL import Image
 def resize_img(file):
     img = Image.open(file)
     width, height = img.size
+    white_width = 0
+    white_height = 0
+    while (white_width < width or white_height < height):
+        white_height += 9
+        white_width += 16
