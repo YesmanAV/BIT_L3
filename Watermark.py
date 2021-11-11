bc0266780c4041acf77d7343672a3a5fb2c02daa
YesmanAV <esmans@mail.ru>
2021-11-11 19:37:42 +0300

    Adding a watermark with no background

--- Watermark.py
+++ Watermark.py
@@ -3,6 +3,10 @@ from PIL import Image
 def watermark_photo(input_image_path, watermark_image_path, output_image_path, position):
     base_image = Image.open(input_image_path)
     watermark = Image.open(watermark_image_path)
-    base_image.paste(watermark, position)
-    base_image.show()
-    base_image.save(output_image_path)
+
+    width, height = base_image.size
+    transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+    transparent.paste(base_image, (0, 0))
+    transparent.paste(watermark, position, mask=watermark)
+    transparent.show()
+    transparent.convert('RGB').save(output_image_path)
