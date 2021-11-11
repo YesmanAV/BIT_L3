aab2753ae8b13f52e5192974d27d487558237638
Kirill Yurcnenko <rikitiki221@mail.ru>
2021-11-11 17:51:32 +0300

    Add server part to mid_server

--- Mid_Serv.py
+++ Mid_Serv.py
@@ -1,1 +1,24 @@
-print()
+from flask import Flask, request, Response
+import numpy as np
+from cv2 import cv2
+import os
+import requests
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/api/test2', methods=['POST'])
+def test():
+    r = request
+
+    nparr = np.fromstring(r.data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # resize here
+    img_resized = img
+
+    return Response(img_resized.tostring())
+
+
+app.run(host="0.0.0.0", port=5000)
