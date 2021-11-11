122386a28ac054d5491cdf43cbbe53d253748344
YesmanAV <esmans@mail.ru>
2021-11-11 21:10:27 +0300

    Added saving of the image and adding to the table

--- Watermark.py
+++ Watermark.py
@@ -1,6 +1,8 @@
 from PIL import Image
+from save_instruments import save_edited_image
+from save_instruments import add_to_edited_table
 
-def watermark_photo(input_image_path, watermark_image_path, output_image_path, position):
+def watermark_photo(input_image_path, watermark_image_path, output_name, position):
     base_image = Image.open(input_image_path)
     watermark = Image.open(watermark_image_path)
 
@@ -8,5 +10,5 @@ def watermark_photo(input_image_path, watermark_image_path, output_image_path, p
     transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
     transparent.paste(base_image, (0, 0))
     transparent.paste(watermark, position, mask=watermark)
-    transparent.show()
-    transparent.convert('RGB').save(output_image_path)
+    save_edited_image(transparent.convert('RGB'), output_name)
+    add_to_edited_table(output_name)
