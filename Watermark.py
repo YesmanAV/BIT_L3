44a5f1c9889d347a780fd7b098d001e0d14be6cb
YesmanAV <esmans@mail.ru>
2021-11-12 14:42:17 +0300

    Processed the size of the watermark

--- Watermark.py
+++ Watermark.py
@@ -11,8 +11,20 @@ def watermark_photo(input_image_path, watermark_image_path, output_name, positio
 
     if base_image.size > watermark.size:
         transparent.paste(watermark, position, mask=watermark)
-        save_edited_image(transparent.convert('RGB'), output_name)
-        add_to_edited_table(output_name)
-        print('The watermark was successfully applied to the picture')
     else:
-        print('Error!!! The size of the watermark is larger than the original photo!')
+        widthWM, heightWM = watermark.size
+        width_new, height_new = find_size(width, height, widthWM, heightWM)
+        transparent.paste(watermark.resize((height_new, width_new), Image.ANTIALIAS), position,
+                          mask=watermark.resize((height_new, width_new), Image.ANTIALIAS))
+    save_edited_image(transparent.convert('RGB'), output_name)
+    add_to_edited_table(output_name)
+
+
+def find_size(w, h, wWM, hWM):
+    if w > h:
+        heightWM_new = int(h / 2)
+        widthWM_new = int((heightWM_new * wWM) / hWM)
+    else:
+        widthWM_new = int(w / 2)
+        heightWM_new = int((widthWM_new * hWM) / wWM)
+    return widthWM_new, heightWM_new
