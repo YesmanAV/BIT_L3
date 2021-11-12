a3c758efe4a95c8aa9266ff8b8b8914550b7c20b
YesmanAV <esmans@mail.ru>
2021-11-12 14:56:17 +0300

    Final version

--- Watermark.py
+++ Watermark.py
@@ -6,16 +6,12 @@ def watermark_photo(input_image_path, watermark_image_path, output_name, positio
     base_image = Image.open(input_image_path)
     watermark = Image.open(watermark_image_path)
     width, height = base_image.size
+    widthWM, heightWM = watermark.size
     transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
     transparent.paste(base_image, (0, 0))
-
-    if base_image.size > watermark.size:
-        transparent.paste(watermark, position, mask=watermark)
-    else:
-        widthWM, heightWM = watermark.size
-        width_new, height_new = find_size(width, height, widthWM, heightWM)
-        transparent.paste(watermark.resize((height_new, width_new), Image.ANTIALIAS), position,
-                          mask=watermark.resize((height_new, width_new), Image.ANTIALIAS))
+    width_new, height_new = find_size(width, height, widthWM, heightWM)
+    transparent.paste(watermark.resize((height_new, width_new), Image.ANTIALIAS), position,
+                      mask=watermark.resize((height_new, width_new), Image.ANTIALIAS))
     save_edited_image(transparent.convert('RGB'), output_name)
     add_to_edited_table(output_name)
 
