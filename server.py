585a2e8b1852804f45f1e14cff9b66a4043780df
DavydovMI <mapkgold@gmail.com>
2021-11-16 12:20:11 +0300

    Fix imports and add watermark function on server

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 from flask import Flask, request, Response
 import numpy as np
 from cv2 import cv2
-from make_watermark_on_photo import watermark_photo
+from Watermark import watermark_photo
 from save_instruments import save_original_image, add_to_origin_table
 import os
 
 # Creating flask server
 app = Flask(__name__)
 app.debug = False
-
 
 @app.route('/api/test', methods=['POST'])
 def test():
@@ -20,12 +19,17 @@ def test():
     counter = save_original_image(img, filename)
     extension = filename.find('.')
     add_to_origin_table(filename)
-    path_to_original = os.path.abspath(os.curdir) + '\\Original\\' + filename[:extension] + str(counter) + filename[
-                                                                                                           extension:]
+    path_to_original = os.path.abspath(os.curdir) + '\\Original\\' + filename[:extension] + str(counter) + filename[extension:]
     edited_name = 'edited.jpg'
 
     path_to_watermark = "w.png"
 
+    watermark_photo(path_to_original, path_to_watermark, edited_name, position=(1, 1))
+    extension = edited_name.find('.')
+    edited_name = edited_name[:extension] + str(counter) + edited_name[extension:]
+    path_to_edited = os.path.abspath(os.curdir) + '\\Edited\\' + edited_name
+    img = cv2.imread(path_to_edited)
+    _, img_encoded = cv2.imencode('.jpg', img)
     return Response(img_encoded.tostring())
 
 
