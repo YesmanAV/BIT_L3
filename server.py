30113e929551f59f88ae68c87be898fad14a9567
DavydovMI <mapkgold@gmail.com>
2021-11-16 12:06:08 +0300

    Comment for flask server

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ from cv2 import cv2
 from make_watermark_on_photo import watermark_photo
 from save_instruments import save_original_image, add_to_origin_table
 import os
-
+# Creating flask server
 app = Flask(__name__)
 app.debug = False
+# run flask server
 app.run(host="0.0.0.0", port=5000)
