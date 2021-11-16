78134c48c57576e8ee4bb3a85bb156992d01fb10
Kirill Yurcnenko <rikitiki221@mail.ru>
2021-11-16 16:54:33 +0300

    Fixed error 2

--- Mid_Serv.py
+++ Mid_Serv.py
@@ -3,6 +3,8 @@ import numpy as np
 from cv2 import cv2
 from resize_image import resize_img
 import requests
+from save_instrument import save_original_image
+import os
 
 
 def get_watermark_image(img):
@@ -14,7 +16,7 @@ def get_watermark_image(img):
         _, img_encoded = cv2.imencode('.jpg', img)
         r = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
         r.raise_for_status()
-        nparr = np.fromstring(str(r.content, 'utf-8'), np.uint8)
+        nparr = np.fromstring(r.content, np.uint8)
         im = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         return im
     except requests.exceptions.HTTPError as e:
@@ -34,11 +36,25 @@ app.debug = False
 @app.route('/api/test', methods=['POST'])
 def test():
     r = request
-    nparr = np.fromstring(str(r.data, 'utf-8'), np.uint8)
+    nparr = np.fromstring(r.data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     img_resized = resize_img(img)
-    result_image = get_watermark_image(img_resized)
-    return Response(result_image.tostring())
+
+    filename = 'tmp.jpg'
+    counter = save_original_image(img_resized, filename)
+    extension = filename.find('.')
+    path_to_original = os.path.abspath(os.curdir) + '\\Original\\' + filename[:extension] + str(counter) + filename[
+                                                                                                           extension:]
+    imgnew = cv2.imread(path_to_original)
+    result_image = get_watermark_image(imgnew)
+
+    counter = save_original_image(result_image, filename)
+    extension = filename.find('.')
+    path_to_original = os.path.abspath(os.curdir) + '\\Original\\' + filename[:extension] + str(counter) + filename[
+                                                                                                           extension:]
+    img2 = cv2.imread(path_to_original)
+    _, img_encoded = cv2.imencode('.jpg', img2)
+    return Response(img_encoded.tostring())
 
 
 app.run(host="0.0.0.0", port=3000)
